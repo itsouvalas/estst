cbc7efb1b65702f30383c7ac0e09d02a7ebb7b7a
itsouvalas <itsouvalas@engineyard.com>
2017-09-20 07:58:29 +0000

    disable before_restart deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "cp #{config.shared_path}/config/secrets.yml #{config.current_path}/config/secrets.yml"
+#run "cp #{config.shared_path}/config/secrets.yml #{config.current_path}/config/secrets.yml"
